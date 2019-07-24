c748c411c1f35af606ddf3223ab741e6bb255760
Ernie Bello <ernie@bello.nyc>
2019-07-24 18:32:39 -0400

    passing tests

--- setup-tests.js
+++ setup-tests.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import 'jest-styled-components';
-import 'jest-dom/extend-expect';
+import '@testing-library/jest-dom/extend-expect';
 // this is basically: afterEach(cleanup)
 import '@testing-library/react/cleanup-after-each';
